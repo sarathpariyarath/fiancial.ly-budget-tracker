514cfc832d69af56cb70a06a428e11de11776a00
sarathpariyarath <sarath.pariyarath@gmail.com>
2021-12-04 13:01:40 +0530

    Initial Commit

---
+++ financially/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  financially
+//
+//  Created by Sarath P on 04/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
