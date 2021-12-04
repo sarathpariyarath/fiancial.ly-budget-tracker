91b9f19d8b65fd78dcb2b4c7a802c9d0fb480981
sarathpariyarath <sarath.pariyarath@gmail.com>
2021-12-04 22:19:28 +0530

    Category section added

---
+++ financially/Controller/NewCategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewCategoryViewController.swift
+//  financially
+//
+//  Created by Sarath P on 04/12/21.
+//
+
+import UIKit
+
+class NewCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "CATEGORIES"
+
+        
+    }
+   
+
+}
