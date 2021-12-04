bdf5d0b6a182fc048c166f7b6770b012511ae618
sarathpariyarath <sarath.pariyarath@gmail.com>
2021-12-04 18:45:40 +0530

    Category Picker Added

--- financially/Controller/AddTransactionViewController.swift
+++ financially/Controller/AddTransactionViewController.swift
@@ -8,19 +8,33 @@
 import UIKit
 
 class AddTransactionViewController: UIViewController {
-    //Sample Picker
+   
+    var categoryItems = ["MacBook", "Food", "Party", "Petrol", "Car Loan", "EMI", "Biryani", "Clothes"]
+    @IBOutlet weak var categoryPicker: UIPickerView!
     @IBOutlet weak var titleTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "ADD TRANSACTIONS"
-        
         titleTextField?.layer.cornerRadius = 9.0
+        categoryPicker.delegate = self
+        categoryPicker.dataSource = self
         
     }
     
+}
 
+extension AddTransactionViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryItems.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryItems[row]
+    }
 }
 
 
