0a08ac9d4611ab544fce48627a5e223041d6ffee
sarathpariyarath <sarath.pariyarath@gmail.com>
2021-12-13 09:47:01 +0530

    Expense Category Added

--- financially/Controller/ExpenseCategoryViewController.swift
+++ financially/Controller/ExpenseCategoryViewController.swift
@@ -8,34 +8,84 @@
 import UIKit
 
 class ExpenseCategoryViewController: UIViewController {
+    let context = CoreDataManager.shared.persistentContainer.viewContext
+    var expenseCategory: [ExpenseCategory]?
     @IBOutlet weak var expenseCategoryTable: UITableView!
-    var categoryItems = ["MacBook", "Food", "Party", "Petrol", "Car Loan", "EMI", "Biryani", "Clothes"]
+    @IBOutlet weak var expenseCategoryTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "EXPENSE CATEGORIES"
         expenseCategoryTable.delegate = self
         expenseCategoryTable.dataSource = self
+        fetchExpenseTransactions()
     }
     
-
+    @IBAction func addExpenseCategoryClicked(_ sender: Any) {
+        let expenceCategory = ExpenseCategory(context: self.context)
+        expenceCategory.categoryName = expenseCategoryTextField.text
+        expenseCategoryTextField.text = ""
+        expenseCategoryTextField.endEditing(true)
+        do {
+            try self.context.save()
+        } catch {
+            print("Failed to save income category")
+        }
+        self.fetchExpenseTransactions()
+        expenseCategoryTable.reloadData()
+        
+    }
+    func fetchExpenseTransactions() {
+        //fetch saved data from database
+        
+        do {
+            self.expenseCategory = try context.fetch(ExpenseCategory.fetchRequest())
+            
+        } catch {
+            print("error \(error.localizedDescription)")
+        }
+    }
+    
 }
-extension ExpenseCategoryViewController: UITableViewDelegate, UITableViewDataSource {
+extension ExpenseCategoryViewController: UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categoryItems.count
+        return expenseCategory?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "expenseCategoryCell", for: indexPath)
-        cell.textLabel?.text = categoryItems[indexPath.row]
+        let list = expenseCategory?[indexPath.row]
+        cell.textLabel?.text = list!.categoryName
         return cell
     }
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, handler) in
-                    print("Delete Action Tapped")
+            print("Delete Action Tapped")
+            let category = self.expenseCategory?[indexPath.row]
+            self.context.delete(category!)
+            
+            do {
+                try self.context.save()
+                
+            } catch{}
+            //refetch the data
+            
+            self.fetchExpenseTransactions()
+            self.expenseCategoryTable.reloadData()
+            for i in 0 ..< self.expenseCategory!.count {
+                let list = self.expenseCategory![i]
+                print(list.categoryName!)
+                   
+                    
+                }
                 }
                 deleteAction.backgroundColor = .red
                 let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
                 return configuration
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        expenseCategoryTextField.endEditing(true)
+        print(expenseCategoryTextField.text!)
+        return true
     }
     
 }
